f7c89b8b074d7baea20bcf4caa848bdfc21ec2fb
Bhanu <Malideedu.Bhanuprakash@skillassure.com>
2025-02-12 23:33:20 +0530

    Feb12

--- src/main/java/com/scaler/productservicefeb25/ProductServiceFeb25Application.java
+++ src/main/java/com/scaler/productservicefeb25/ProductServiceFeb25Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServiceFeb25Application {
+public class ProductServiceFeb25Application
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(ProductServiceFeb25Application.class, args);
+
+        System.out.println("Hello World");
     }
 
 }
