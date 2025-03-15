17335837366c3ca7692bea2adfd79b4c65e8e3e1
Bhanu <Malideedu.Bhanuprakash@skillassure.com>
2025-03-16 00:30:45 +0530

    Response Entity

--- src/main/java/com/scaler/productservicefeb25/controllers/ProductController.java
+++ src/main/java/com/scaler/productservicefeb25/controllers/ProductController.java
@@ -4,6 +4,8 @@ package com.scaler.productservicefeb25.controllers;
 import com.scaler.productservicefeb25.models.Product;
 import com.scaler.productservicefeb25.services.FakeStoreProductService;
 import com.scaler.productservicefeb25.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,13 +32,15 @@ public class ProductController
 
     // http://loacalhost:8080/Products/1 => GET
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long id)
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id)
     {
-        Product singleProduct = productService.getProductById(id);
-        return singleProduct;
+        Product product= productService.getProductById(id);
+        return new ResponseEntity<>(
+                product, HttpStatus.NOT_FOUND);// OK
     }
 
     // http://localhost:8080/Products
+    // http://localhost:8080/Products/20
     @GetMapping()
     public List<Product> getAllProducts()
     {
